128ed02735d429d0aecb46070ea4891cb320599a
s0npaRi11 <paragmahale36@gmail.com>
2021-01-30 12:43:46 +0530

    auth-token header aloowed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, auth-token"
   );
   next();
 });
