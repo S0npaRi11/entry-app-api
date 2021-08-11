f674011d28e153683ea2700d0b148c07f19f4d53
s0npaRi11 <paragmahale36@gmail.com>
2021-08-11 11:50:43 +0530

    cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "[http://localhost:3000, http://localhost:3000/register]");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, auth-token"
