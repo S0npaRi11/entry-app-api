1802fa8bf23785afb12842222387e5528dcbd2bb
s0npaRi11 <paragmahale36@gmail.com>
2021-08-11 11:56:38 +0530

    cors

--- server.js
+++ server.js
@@ -15,17 +15,19 @@ database()
 
 
 //app.use()'s
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "[http://localhost:3000, http://localhost:3000/register]");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, auth-token"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "[http://);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, auth-token"
+//   );
+//   next();
+// });
 app.use(useragent.express())
 
 
